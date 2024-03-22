b8e46f14bf0ceb1b3634d3b76a14f45d060f8894
Wawan Kurniawan <wawankurniawan31@gmail.com>
2024-03-22 10:22:08 +0700

    update name apps

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Bahri Klinik</a>
+            <a href="index.html">Jasmine Klinik</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
